54afb1c28fda350cdaf0cb2f5eb83b93fdced784
Mi13285 <mixkud95@inbox.ru>
2024-12-07 12:05:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function towerBuilder(nFloors) {
+  let space,
+    star,
+    tower = [];
+  for (let i = 1; i <= nFloors; i++) {
+    space = "".repeat(nFloors - i);
+    star = "*".repeat(2 * i - 1);
+    tower.push(`${space}${star}${space}`);
+  }
+  return tower;
+}
+console.log(towerBuilder(3));
+console.log(towerBuilder(6));
